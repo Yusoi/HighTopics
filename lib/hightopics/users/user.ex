9b2fdb32677bab9ac3923b0965c80391973ce31c
ZePedroResende <j.p.resende68@gmail.com>
2018-02-10 15:38:20 +0000

    Revert "added topics to user"
    Revert on commit because interference with phoenix file name

--- lib/hightopics/users/user.ex
+++ lib/hightopics/users/user.ex
@@ -2,13 +2,11 @@ defmodule Hightopics.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
   alias Hightopics.Users.User
-  alias Hightopics.Topics.Topic
 
 
   schema "users" do
     field :email, :string
     field :username, :string
-    has_many :topics, Topic
 
     timestamps()
   end
@@ -16,7 +14,7 @@ defmodule Hightopics.Users.User do
   @doc false
   def changeset(%User{} = user, attrs) do
     user
-    |> cast(attrs, [:username, :email, :topics])
-    |> validate_required([:username, :email, :topics])
+    |> cast(attrs, [:username, :email])
+    |> validate_required([:username, :email])
   end
 end
